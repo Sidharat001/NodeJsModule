0e96624b81a7906d5fb6615de05d3430c8d2bf83
Karthik <dr.sidharat@gmail.com>
2024-07-11 23:56:28 +0530

    Add JWT Topic

--- Cluster/index.js
+++ Cluster/index.js
@@ -1,9 +1,32 @@
-const http = require('http');
+// step 1: import all required packages
+const cluster = require('cluster');
+const express = require('express');
+const os = require('os');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "application/json"});
-    // res.write(JSON.parse{'message': 'Server running at http://localhost:8080', 'processId': process.pid});
-    res.end();
-});
+// step 2: get length of cpu
+let totalCpu = os.availableParallelism();
 
-server.listen(8080, () => console.log('Server listening on port 3000'));
+// step 3: create workloader instance
+if(cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+    for(let i = 0; i < totalCpu; i++) {
+        cluster.fork();
+    }
+}else {
+    // step 4: create connection
+    const app = express();
+
+    // step 5: run express server
+    app.get('/', (req, res) => {
+        res.write('Cluster Series ' + process.pid);
+        res.send();
+    })
+
+    app.get('/api/', (req, res) => {
+        res.write('Cluster Series ' + process.pid);
+        res.send();
+    });
+
+    // step 6: listen on port
+    app.listen(8000, () => console.log('listening on http://localhost:8000'));
+}
---
+++ Jwt/controller/User.Controller.js
@@ -0,0 +1,19 @@
+const UserEntity = require('../model/user.entity');
+class UserService {
+    async registerUser(req, res) {
+        try {
+            let { name, last, password, gander } = req.body;
+            if(!name || !last || !gander || !gander) { 
+                res.render('register', {"title": "sign-in", "message": "All fields are required"});
+            }
+            const result = await UserEntity.create({name, last, password, gander});
+            if(result) 
+            res.redirect(201,'/');
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+}
+
+module.exports = new UserService();
